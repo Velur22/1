cde1160eb8e4b568af958a1fba96e99884f83308
Velur22 <vailorsteam@gmail.com>
2024-08-07 00:51:21 +0400

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,30 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
+
+first = 42
+second = 69
+third = 42
+if first == second == third:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
+
+first = 59
+second = 59
+third = 59
+if first == second == third:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
+
